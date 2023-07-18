ce90076ed5f8e43c72d1559831dd2906b6bdc50c
Zack Harris <zack.idea@gmail.com>
2023-07-18 07:39:56 -0400

    Added ImGUI library, VST now not working in host

--- TestDelayUI.cs
+++ TestDelayUI.cs
@@ -9,6 +9,7 @@ public class TestDelayUI : IAudioPluginView
 {
 	public ViewRectangle Size => throw new NotImplementedException();
 
+	// Todo: fix these functions later, just adding test implementations for now
 	public void Attached(nint parent, AudioPluginViewPlatform type)
 	{
 		throw new NotImplementedException();
@@ -16,61 +17,67 @@ public class TestDelayUI : IAudioPluginView
 
 	public bool CanResize()
 	{
-		throw new NotImplementedException();
+		return true;
 	}
 
 	public bool CheckSizeConstraint(ViewRectangle rect)
 	{
-		throw new NotImplementedException();
+		return true;
 	}
 
 	public bool IsPlatformTypeSupported(AudioPluginViewPlatform platform)
 	{
-		throw new NotImplementedException();
+		//if (platform == AudioPluginViewPlatform.Hwnd)
+		//{
+		//	return true;
+		//}
+		return true;
 	}
 
 	public void OnFocus(bool state)
 	{
-		throw new NotImplementedException();
+		//throw new NotImplementedException();
 	}
 
 	public void OnKeyDown(ushort key, short keyCode, short modifiers)
 	{
-		throw new NotImplementedException();
+		//throw new NotImplementedException();
 	}
 
 	public void OnKeyUp(ushort key, short keyCode, short modifiers)
 	{
-		throw new NotImplementedException();
+		//throw new NotImplementedException();
 	}
 
 	public void OnSize(ViewRectangle newSize)
 	{
-		throw new NotImplementedException();
+		//throw new NotImplementedException();
 	}
 
 	public void OnWheel(float distance)
 	{
-		throw new NotImplementedException();
+		//throw new NotImplementedException();
 	}
 
 	public void Removed()
 	{
-		throw new NotImplementedException();
+		//throw new NotImplementedException();
 	}
 
 	public void SetContentScaleFactor(float factor)
 	{
-		throw new NotImplementedException();
+		//throw new NotImplementedException();
 	}
 
 	public void SetFrame(IAudioPluginFrame frame)
 	{
-		throw new NotImplementedException();
+		//throw new NotImplementedException();
 	}
 
 	public bool TryFindParameter(int xPos, int yPos, out AudioParameterId parameterId)
 	{
-		throw new NotImplementedException();
+		//throw new NotImplementedException();
+		parameterId = default(AudioParameterId);
+		return false;
 	}
 }
